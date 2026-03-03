79306e0fba7345d1b53d83f799ec1f5ed45e61e7
MhZ9191 <hegic91@gmail.com>
2026-03-03 14:40:26 +0100

    Add routers & controllers

--- controllers/BlogController.js
+++ controllers/BlogController.js
@@ -8,4 +8,66 @@ function index(req, res) {
   });
 }
 
-module.exports = { index };
+function show(req, res) {
+  const searcheditem = postsData.find(
+    (el) => el.id === parseInt(req.params.id),
+  );
+
+  if (!searcheditem) {
+    return res.status(404).json({
+      message: "elemento non trovato",
+      success: false,
+    });
+  }
+
+  res.json({
+    message: "Visualizza un singolo elemento",
+    result: searcheditem,
+    success: true,
+  });
+}
+
+function store(req, res) {
+  res.json({
+    message: "Creo un nuovo elemento",
+    success: true,
+  });
+}
+
+function update(req, res) {
+  res.json({
+    message: "Modifico interamente un elemento",
+    success: true,
+  });
+}
+
+function modify(req, res) {
+  res.json({
+    message: "Modifico parzialmente un elemento",
+    success: true,
+  });
+}
+
+function destroy(req, res) {
+  const indexitem = postsData.findIndex(
+    (el) => el.id === parseInt(req.params.id),
+  );
+
+  if (indexitem === -1) {
+    return res.status(404).json({
+      message: "Elemento non trovato",
+      success: false,
+    });
+  }
+
+  const tmpElement = postsData[indexitem];
+  postsData.splice(indexitem, 1);
+  console.log(postsData);
+  res.json({
+    messagge: "Elemento eliminato",
+    result: tmpElement,
+    success: true,
+  });
+}
+
+module.exports = { index, show, store, update, modify, destroy };
--- routers/posts.js
+++ routers/posts.js
@@ -1,76 +1,12 @@
-const postsData = require("../data/blogsData");
 const express = require("express");
 const posts = express();
 const blogController = require("../controllers/BlogController");
 
-//! Index
 posts.get("/", blogController.index);
-
-//! Show
-posts.get("/:id", (req, res) => {
-  const searcheditem = postsData.find(
-    (el) => el.id === parseInt(req.params.id),
-  );
-
-  if (!searcheditem) {
-    return res.status(404).json({
-      message: "elemento non trovato",
-      success: false,
-    });
-  }
-
-  res.json({
-    message: "Visualizza un singolo elemento",
-    result: searcheditem,
-    success: true,
-  });
-});
-
-//! Store
-posts.post("/", (req, res) => {
-  res.json({
-    message: "Creo un nuovo elemento",
-    success: true,
-  });
-});
-
-//! Update
-posts.put("/:id", (req, res) => {
-  res.json({
-    message: "Modifico interamente un elemento",
-    success: true,
-  });
-});
-
-//! Modify
-posts.patch("/:id", (req, res) => {
-  res.json({
-    message: "Modifico parzialmente un elemento",
-    success: true,
-  });
-});
-
-//! Destroy
-posts.delete("/:id", (req, res) => {
-  const indexitem = postsData.findIndex(
-    (el) => el.id === parseInt(req.params.id),
-  );
-
-  if (indexitem === -1) {
-    return res.status(404).json({
-      message: "Elemento non trovato",
-      success: false,
-    });
-  }
-
-  const tmpElement = postsData[indexitem];
-  postsData.splice(indexitem, 1);
-  console.log(postsData);
-  res.json({
-    messagge: "Elemento eliminato",
-    result: tmpElement,
-    success: true,
-  });
-});
+posts.get("/:id", blogController.show);
+posts.post("/", blogController.store);
+posts.put("/:id", blogController.update);
+posts.patch("/:id", blogController.modify);
+posts.delete("/:id", blogController.destroy);
 
 module.exports = posts;
